3ae2d9dc9e2914fff7d428c1e95a584ce4d2b88c
maxnvdm <maxnvdm@gmail.com>
2019-11-21 15:20:06 +0200

    Trying to get modal incrememnt JS working

--- shopping.js
+++ shopping.js
@@ -1,4 +1,11 @@
 
+let quantity = 0;
 function colorSelect(color){
-    document.getElementById("color-choice").innerHTML = color;
+    document.getElementById("color-choice1").innerHTML = color;
+    document.getElementById("color-choice2").innerHTML = color;
+}
+
+function increment(){
+    quantity++;
+    document.getElementById("quantity-value").outerHTML = quantity;
 }
