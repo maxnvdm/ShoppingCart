57591a4d7f4cffa86296a06a8a8f85d93b8a513b
maxnvdm <maxnvdm@gmail.com>
2019-11-27 14:58:24 +0200

    Refactoring color buttons into classes

--- shopping.js
+++ shopping.js
@@ -2,48 +2,49 @@
 let quantity = 0;
 let cart = 0;
 let details = "";
+
 // Color tracking variables
-// let colors = [red, violet, black, white, orange, yellow, navyblue, turquoise, beige, skyblue, limegreen, pink, cream, emeraldgreen, darkgrey, brown, aquamarine, lightgrey];
-let colors = {
-    "Red": 0,
-    "Violet": 0,
-    "Black": 0,
-    "White": 0,
-    "Orange": 0,
-    "Yellow": 0,
-    "Navy Blue": 0,
-    "Turquoise": 0,
-    "Beige": 0,
-    "Sky Blue": 0,
-    "Lime Green": 0,
-    "Pink": 0,
-    "Cream": 0,
-    "Emerald Green": 0,
-    "Dark Grey": 0,
-    "Brown": 0,
-    "Aqua Marine": 0,
-    "Light Grey": 0,
+let colors = ["Red", "Violet", "Black", "White", "Orange", "Yellow", "Navy Blue", "Turquoise", "Beige", "Sky Blue", "Lime Green", "Pink", "Cream", "Emerald Green", "Dark Grey", "Brown", "Aqua Marine", "Light Grey"];
+let colorclasses;
+
+class Color {
+    constructor(color) {
+        this.name = color;
+        this.quantity = 0;
+    }
+    get button() {
+        let btnName = this.name.toLowerCase().replace(" ","-");
+        return "<button class='inline-block color-button button-" + btnName + "' onclick=colorSelect('" + this.name + "');></button>";
+    }
+    set quantitySet(amount) {
+        this.quantity = amount;
+    }
+    get quantityGet() {
+        return this.quantity;
+    }
 }
 
-let colorbuttons = {
-    "Red": '<button class="inline-block color-button button-red" onclick="colorSelect(\'Red\');"></button>',
-    "Violet": '<button class="inline-block color-button button-violet" onclick="colorSelect(\'Violet\');"></button>',
-    "Black": '<button class="inline-block color-button button-black" onclick="colorSelect(\'Black\');"></button>',
-    "White": '<button class="inline-block color-button button-white" onclick="colorSelect(\'White\');"></button>',
-    "Orange": ' <button class="inline-block color-button button-orange" onclick="colorSelect(\'Orange\');"></button>',
-    "Yellow": '<button class="inline-block color-button button-yellow" onclick="colorSelect(\'Yellow\');"></button>',
-    "Navy Blue": '<button class="inline-block color-button button-navy-blue" onclick="colorSelect(\'Navy Blue\');"></button>',
-    "Turquoise": '<button class="inline-block color-button button-turquoise" onclick="colorSelect(\'Turquoise\');"></button>',
-    "Beige": '<button class="inline-block color-button button-beige" onclick="colorSelect(\'Beige\');"></button>',
-    "Sky Blue": '<button class="inline-block color-button button-sky-blue" onclick="colorSelect(\'Sky Blue\');"></button>',
-    "Lime Green": '<button class="inline-block color-button button-lime-green" onclick="colorSelect(\'Lime Green\');"></button>',
-    "Pink": '<button class="inline-block color-button button-pink" onclick="colorSelect(\'Pink\');"></button>',
-    "Cream": '<button class="inline-block color-button button-cream" onclick="colorSelect(\'Cream\');"></button>',
-    "Emerald Green": '<button class="inline-block color-button button-emerald-green" onclick="colorSelect(\'Emerald Green\');"></button>',
-    "Dark Grey": '<button class="inline-block color-button button-dark-grey" onclick="colorSelect(\'Dark Grey\');"></button>',
-    "Brown": '<button class="inline-block color-button button-brown" onclick="colorSelect(\'Brown\');"></button>',
-    "Aqua Marine": '<button class="inline-block color-button button-aqua-marine" onclick="colorSelect(\'Aqua Marine\');"></button>',
-    "Light Grey": '<button class="inline-block color-button button-light-grey" onclick="colorSelect(\'Light Grey\');"></button>',
+function createColors() {
+    colorclasses = {
+        "Red": new Color("Red"),
+        "Violet": new Color("Violet"),
+        "Black": new Color("Black"),
+        "White": new Color("White"),
+        "Orange": new Color("Orange"),
+        "Yellow": new Color("Yellow"),
+        "Navy Blue": new Color("Navy Blue"),
+        "Turquoise": new Color("Turquoise"),
+        "Beige": new Color("Beige"),
+        "Sky Blue": new Color("Sky Blue"),
+        "Lime Green": new Color("Lime Green"),
+        "Pink": new Color("Pink"),
+        "Cream": new Color("Cream"),
+        "Emerald Green": new Color("Emerald Green"),
+        "Dark Grey": new Color("Dark Grey"),
+        "Brown": new Color("Brown"),
+        "Aqua Marine": new Color("Aqua Marine"),
+        "Light Grey": new Color("Light Grey"),
+    }
 }
 
 function colorSelect(color){
@@ -63,24 +64,28 @@ function decrement(){
         document.querySelector("#quant").innerHTML = quantity;
     } else {
         document.querySelector("#quant").innerHTML = quantity;
-    }
-    
+    }   
 }
 
 function updateCart(){
+    // Fetch the current color choice
     let color = document.getElementById("color-choice2").innerText;
-    colors[color] = quantity;
+    // Set that color's quantity to the selected amount
+    colorclasses[color].quantitySet = quantity;
 
-    // Get the total number of items in the cart
-    cart = sumItems(colors);
+    // Get the total number of colors in the cart
+    cart = sumItems(colorclasses);
+
     // Update the cart text to the total number of items
     document.querySelector("#quantity").innerHTML = cart;
-    quantity = 0;
-    for(var col in colors){
-        if(colors[col]!=0){
-            // console.log(col, colors[col])
-            for (let i = colors[col]; i>0; i--){
-                details +=colorbuttons[col];
+
+    // Set the color icons below DETAILS
+    for(const item of colors){
+        // console.log(item, colorclasses[item])
+
+        if(colorclasses[item].quantityGet != 0){
+            for (let i = colorclasses[item].quantityGet; i > 0; i--){
+                details +=colorclasses[item].button;
             }
         }
     }
@@ -97,8 +102,8 @@ function updateQuant(){
         document.querySelector("#quant").innerText = "0";
         quantity = 0;
     } else {
-        document.querySelector("#quant").innerText = colors[color];
-        quantity = colors[color];
+        document.querySelector("#quant").innerText = colorclasses[color].quantityGet;
+        quantity = colorclasses[color].quantityGet;
     }
     
 }
@@ -110,7 +115,7 @@ function resetQuantity(){
 function sumItems(obj) {
     let sum = 0;
     for( var col in obj){
-        sum+= obj[col];
+        sum+= obj[col].quantityGet;
     }
     return sum;
 }
